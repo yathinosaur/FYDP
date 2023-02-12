3380a3aa02b89368aed24498d3e05742a45f1521
Yathindra Shivakumar <32390768+yathinosaur@users.noreply.github.com>
2023-02-12 14:03:20 -0500

    Unity changes

---
+++ FYDP Project 2/Assets/Scripts/HaptArmVR.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HaptArmVR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetRequest()
+    {
+        StartCoroutine(GetRequest("http://127.0.0.1:5000/angle"));
+    }
+
+    private IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
